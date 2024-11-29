8a1f3ebf6e8a821e569a5edf93fe58883661f39c
Prithak N Koirala <122518784+pp-beb@users.noreply.github.com>
2024-11-29 18:41:18 +0545

    shows error but we'll work on it :]

--- expense.cpp
+++ expense.cpp
@@ -17,16 +17,42 @@ class everyDay {
 			days=D;
 		}
 		
-		void entryData(){
-			cout<<"How long do you want this app to help you track your responses?(In days)"<<endl;
-			cin>>days;
-			for(int i=0;i<days;i++){
-			cout<<"___________Recording the data for day"<<i<<"___________"<<endl;
-			}
-		}
+		
 	};
 	
+	int entryDays(){
+		int n;
+			cout<<"How long do you want this app to help you track your responses?(In days)"<<endl;
+			cin>>n;
+			return n;
+		}
+	
+	void entryData(int n,everyDay a[])
+	{
+		int i=0;
+		if (i<=n-1)
+		{
+		cout<<"___________Recording the data for day"<<i+1<<"___________"<<endl;
+		}
+		else
+		{
+			cout<<"Your tarcking days are complete, generating report:";
+		}
+		cout<<"How much money did you spend today?"<<endl;
+		cin>>a[i].spent;
+		cout<<"How much money did you make today?"<<endl;
+		cin>>a[i].income;
+		}
+		void calculations(everyDay a[])
+		{
+			cout<<"ypu spent thismuch today";
+		}
+		
 	int main()
 	{
+	int days=entryDays();
+	everyDay* OK = new everyDay[days];
+	entryData(days,OK);
+	calculations(OK);
 		return 0;
 	}
